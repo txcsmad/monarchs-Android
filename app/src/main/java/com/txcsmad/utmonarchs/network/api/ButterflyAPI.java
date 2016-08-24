2cc1a29ad0ef0d5d6dce9594e878137d3b28d3a2
Jacob Lubecki <jacoblubecki@gmail.com>
2016-08-24 14:19:16 -0500

    Update build.gradle and improve layout.

--- app/src/main/java/com/txcsmad/utmonarchs/network/api/ButterflyAPI.java
+++ app/src/main/java/com/txcsmad/utmonarchs/network/api/ButterflyAPI.java
@@ -10,8 +10,8 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Jacob on 4/23/16.
--- app/src/main/java/com/txcsmad/utmonarchs/network/models/Butterfly.java
+++ app/src/main/java/com/txcsmad/utmonarchs/network/models/Butterfly.java
@@ -11,9 +11,25 @@ public class Butterfly {
      * Base-64 encoded image (JPEG).
      */
     public String image;
+
+    /**
+     * Identification for user that uploaded the butterfly.
+     */
     public String userId;
     public String classId;
+
+    /**
+     * Longitude of the butterfly.
+     */
     public String longitude;
+
+    /**
+     * Latitude of the butterfly.
+     */
     public String latitude;
+
+    /**
+     * User's notes on the butterfly.
+     */
     public String notes;
 }
